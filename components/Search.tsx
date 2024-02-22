ad8eb36375d1f3324dceda2ba2ee4ead2dbd4ab5
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-02-22 17:56:40 -0500

    modify clarity

--- components/Search.tsx
+++ components/Search.tsx
@@ -138,6 +138,7 @@ export const Search: FC<SearchProps> = ({ onSearch, onAnswerUpdate, onDone }) =>
         <div className="flex items-center justify-center pt-64 sm:pt-72 flex-col">
           <div className="inline-block h-16 w-16 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"></div>
           <div className="mt-8 text-2xl">Getting answer...</div>
+          <div id="demo-chat-ad" className="ad flex justify-end w-full border-b border-zinc-800 pb-4"></div>
         </div>
       ) : (
         <div className="mx-auto flex h-full w-full max-w-[750px] flex-col items-center space-y-6 px-3 pt-32 sm:pt-64">
